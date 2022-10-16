48b36f236f25d7f18ee88352a5dff8261b8f5614
idanigor <idanigor@gmail.com>
2022-10-16 23:05:25 +0600

    feat: add binary-serch, queue, remove-from-list and stack

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js')
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
 class Queue {
+	constructor() {
+		this.head = null
+		this.tail = null
+		this.length = 0
+	}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	getUnderlyingList() {
+		return this.head
+	}
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	enqueue(value) {
+		let newNode = new ListNode(value)
+		if (!this.head || !this.tail) {
+			this.head = newNode
+			this.tail = newNode
+		} else {
+			this.tail.next = newNode
+			this.tail = newNode
+		}
+		this.length++
+	}
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	dequeue() {
+		let res
+		if (this.length !== 0) {
+			res = this.head
+			this.head = this.head.next
+		}
+		return res.value
+	}
 }
 
+
+
+// const queue = new Queue()
+// queue.enqueue(5)
+// queue.enqueue(6)
+// queue.enqueue(7)
+// queue.dequeue() //5
+// queue.dequeue() //6
+// const idanigor = new Queue()
+// idanigor.enqueue(3)
+// idanigor.enqueue(6)
+// idanigor.dequeue()
+// // idanigor.enqueue(7)
+
+// console.log(JSON.stringify(queue))
+
+
+
 module.exports = {
-  Queue
-};
+	Queue,
+}
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-// const { ListNode } = require('../extensions/list-node.js');
+//const { ListNode } = require('../extensions/list-node.js')
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+// const linkListObj = {
+// 	value: 3,
+// 	next: {
+// 		value: 1,
+// 		next: {
+// 			value: 2,
+// 			next: { value: 3, next: { value: 4, next: { value: 5, next: null } } },
+// 		},
+// 	},
+// }
+
+function removeKFromList(l, k) {
+	// let arr = []
+	// let obj = {}
+	// function getProp(o, k) {
+	// 	for (let prop in o) {
+	// 		if (typeof o[prop] === 'object') {
+	// 			getProp(o[prop], k)
+	// 		} else {
+	// 			if (o[prop] !== k) {
+	// 				arr.push(o[prop])
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// getProp(l, k)
+	// function getList() {
+	// 	let current = obj
+	// 	for (let i = 0; i < arr.length; i++) {
+	// 		current.value = arr[i]
+	// 		if (i === arr.length - 1) {
+	// 			current.next = null
+	// 		} else {
+	// 			current.next = {}
+	// 			current = current.next
+	// 		}
+	// 	}
+	// 	return obj
+	// }
+	// getList()
+	// return obj
+
+	let current = l
+	let prev = null
+
+	while (current) {
+		if (current.value === k) {
+			if (prev) {
+				prev.next = current.next
+			} else {
+				l = current.next
+			}
+			current = l.next
+		} else {
+			prev = current
+			current = current.next
+		}
+	}
+
+	return l
 }
 
+//console.log(JSON.stringify(removeKFromList(linkListObj, 3)))
+
 module.exports = {
-  removeKFromList
-};
+	removeKFromList,
+}
